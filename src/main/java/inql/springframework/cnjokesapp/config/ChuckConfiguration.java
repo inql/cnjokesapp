95ee293587019c90db1945245f19a1353c4b1066
Dawid <dbinkus96@gmail.com>
2017-10-09 17:26:10 +0200

    added java configuration file and changed JokeServiceImpl to be constructor injected service

---
+++ src/main/java/inql/springframework/cnjokesapp/config/ChuckConfiguration.java
@@ -0,0 +1,21 @@
+package inql.springframework.cnjokesapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Project: cnjokesapp
+ * Created by: inql
+ * on 09.10.17
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+
+
+}
